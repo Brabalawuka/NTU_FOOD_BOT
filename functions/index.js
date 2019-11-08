5f307debaa1fd97dd1b79a934f2022364a4fde80
Brabalawuka <lyuz0001@e.ntu.edu.sg>
2019-11-08 18:46:23 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@
 //firebase functions:config:set wave.uid='IbVuxpPUaaeZ0NUbvzHaulOzTJP2'
 
 const fs = require('fs');
+const os = require('os');
+const path = require('path');
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const Telegraf = require('telegraf');
@@ -10,16 +12,24 @@ const Composer = require('telegraf/composer');
 const session = require('telegraf/session');
 const Stage = require('telegraf/stage');
 const Scene = require('telegraf/scenes/base');
+const Telegram = require('telegraf/telegram');
+const https = require('https');
 
-
+//Initialise app
 admin.initializeApp(functions.config().firebase);
+
+//FireStore instance
 let db = admin.firestore();
-let myBucket = admin.storage().bucket();
+
+//Bucket instance
+let myBucket = admin.storage().bucket("wave-sandbox-1dbd5.appspot.com");
 
 
 const beginning = "Entered information : \n\n";
-const bot = new Telegraf(functions.config().bot.token);
-const { enter, leave } = Stage;
+
+//bot instance
+const bot = new Telegraf("965613179:AAHmrkgg_Z3RTX7IM9fm6lr2_W0TOz-zNo0");
+
 const stage = new Stage();
 
 const webHook = 'https://api.telegram.org/bot965613179:AAHmrkgg_Z3RTX7IM9fm6lr2_W0TOz-zNo0/setWebhook?url=https://us-central1-wave-sandbox-1dbd5.cloudfunctions.net/helloWorld'
@@ -45,10 +55,12 @@ bot.use(stage.middleware());
 
 //Starting sequence
 bot.start((ctx) => {
+  console.log(ctx.from)
     ctx.reply(
       'Enter food type:  eg. buffet,dessert',
-      { reply_markup: { remove_keyboard: true } }  
+    
     );
+  
     ctx.scene.enter('getFoodType');
   })
 
@@ -67,6 +79,7 @@ getFoodType.command('start', async (ctx) => {
 
 
 getFoodType.on('text', async (ctx) => {
+
     if (ctx.message.text === '◀️ Back') {
         await ctx.scene.leave('getFoodType')
         ctx.reply('Quit new free food registration. \nYou can enter /start anytime to start over')
@@ -229,24 +242,12 @@ getDescrption.on('text', async (ctx) => {
         if (ctx.session.description === null){
             return ctx.reply('Please enter free food description:')
         } else {
-        
-    
 
-            ctx.reply(
-                beginning +
-                `Food type:  ${ctx.session.foodType} \n` +
-                `Location: ${ctx.session.location} \n` +
-                `Estimated Ending: ${ctx.session.endTime} \n` +
-                `Description: ${ctx.session.description} \n\n` +
-                'Note that you have reached the last step, if you have any photo for the food, upload a photo now to finish the post\n\nOR\n\n' + 
-                'you can click Next ▶ button to finish without a photo.\n\n' + 
-                'By finishing, you agree to let bot to represent you to post to food channel and pls do not post misleading info',
-                { reply_markup: { keyboard: [['◀️ Back', 'Next ▶']], resize_keyboard: true, one_time_keyboard: true } }
-                )
+          ctx.reply('Send me a photo of the food or you can send any text if you dont have photo :')
 
 
-            await ctx.scene.leave('getDescription')
-            ctx.scene.enter('getPic')
+          await ctx.scene.leave('getDescription')
+          ctx.scene.enter('getPic')
 
 
 
@@ -279,12 +280,15 @@ getPic.command('start', async (ctx) => {
   ctx.session.location = null
   ctx.session.endTime = null
   ctx.session.description = null
+  ctx.session.hasImage = null
   await ctx.scene.leave('getPic')
   ctx.scene.enter('getFoodType')
 })
 
 
 getPic.on('text', async (ctx) => {
+  
+
     if (ctx.message.text === '◀️ Back') {
         await ctx.scene.leave('getPic')
         ctx.scene.enter('getDescription')
@@ -300,50 +304,58 @@ getPic.on('text', async (ctx) => {
         return
 
     } else if (ctx.message.text === 'Next ▶') {
-        ctx.reply(
-            beginning +
-            `Food type:  ${ctx.session.foodType} \n` +
-            `Location: ${ctx.session.location} \n` +
-            `Estimated Ending: ${ctx.session.endTime} \n` +
-            `Description: ${ctx.session.description} \n\n` +
-            'Sending the post.... Please Wait \n'
-            )
+      if (ctx.session.hasImage === null){
+        return ctx.reply('Please do not skip photo upload session, type in other words if u dont have a photo')
 
-        //await createPostOnChannel();
-
-
-        //Create post on our wave discsussion channel
-        var createdTime = admin.firestore.Timestamp.now();
-        var documentName = createdTime.toMillis()+"+"+functions.config().wave.uid;
+      } else {
+        if (!ctx.hasImage){
+          var createdTime = admin.firestore.Timestamp.now();
+          ctx.session.createdTime = createdTime
+          ctx.session.documentID = createdTime.toMillis()+"+"+"IbVuxpPUaaeZ0NUbvzHaulOzTJP2";
+        }
+       
         var content = 
           `Estimated Ending: ${ctx.session.endTime} \n` +
-          `Posted by: @${ctx.from.id} \n` +
+          `Posted by: @${ctx.from.username === null ? ctx.from.first_name : ctx.from.username} \n` +
           `Food type:  ${ctx.session.foodType} \n` +
           `Location: ${ctx.session.location} \n` +
           `Description: ${ctx.session.description} \n`
         let foodinfo = {
           'creatorID' : 'Wave Admin',
           'isAnonymous' : false,
-          'createdTime' : createdTime,
+          'createdTime' : ctx.session.createdTime,
           'likedPeople' : {},
           'title' : `${ctx.session.foodType} at ${ctx.session.location}`,
           'content' : content,
           'category' : 'Food',
-          'creatorUID' : functions.config().wave.uid,
-          'hasImage' : false,
+          'creatorUID' : "IbVuxpPUaaeZ0NUbvzHaulOzTJP2",
+          'hasImage' : ctx.session.hasImage,
           'isPinned' : false,
           'isHidden' : false,
           'commentCount' : 0,
+          'imageURL' : ctx.session.hasImage ? ctx.session.imageUrl : null,
           'likeCount' : 0,
-          'threadID' : documentName,
-
-
+          'threadID' : ctx.session.documentID,
         }
 
-        let postRef = db.collection('discussion').doc('NTU').collection('Food').doc(documentName);
+        // ctx.reply("Still running");
+
+        let postRef = db.collection('discussion').doc('NTU').collection('Food').doc(ctx.session.documentID);
+        // ctx.reply("running 2");
         postRef.set(foodinfo).then(function() {
           ctx.reply('Post created!')
           console.log(`New Post from @${ctx.from.id}`);
+
+      
+          if (ctx.session.hasImage){
+            ctx.telegram.sendPhoto("@wave_bot_test", ctx.session.imageFileId, {caption: content} );
+  
+          } else {
+            ctx.telegram.sendMessage("@wave_bot_test", content);
+
+          }
+
+
           return
 
         }).catch(e => {
@@ -352,98 +364,103 @@ getPic.on('text', async (ctx) => {
         });
 
       
-
+        
         
 
+        // ctx.reply("running 3")
         await ctx.scene.leave('getPic')
-        
-        
+
+        return;
+      }
+ 
 
     }
 
-    
+    ctx.session.hasImage = false
     ctx.reply(
-      'Please send a photo or \n' +
-      'Click Next ▶ button to continue if u dont have any photo for food',
+      beginning +
+      `Food type:  ${ctx.session.foodType} \n` +
+      `Location: ${ctx.session.location} \n` +
+      `Estimated Ending: ${ctx.session.endTime} \n` +
+      `Description: ${ctx.session.description} \n` +
+      `Photo: no photo\n\n` +
+
+      'Note that you have reached the last step, if you still have any photo for the food, upload a photo now \n\nOR\n\n' + 
+      'you can click Next ▶ button to finish without a photo.\n\n' + 
+      'By finishing, you agree to let bot to represent you to post to food channel and pls do not post misleading info',
       { reply_markup: { keyboard: [['◀️ Back', 'Next ▶']], resize_keyboard: true, one_time_keyboard: true } }
       )
+  
     
   })
 
 
   getPic.on('photo', async (ctx) => {
 
-    ctx.replyWithChatAction('typing')
-    
-    
+    var createdTime = admin.firestore.Timestamp.now();
+    ctx.session.createdTime = createdTime;
+    ctx.session.documentID = createdTime.toMillis()+"+"+functions.config().wave.uid;
+    const myImage = path.join(os.tmpdir(), "temp.jpg")
+    const myImageStream = fs.createWriteStream(myImage);
+    console.log(myImage + " path created")
 
-    ctx.reply(
-        beginning +
-        `Food type:  ${ctx.session.foodType} \n` +
-        `Location: ${ctx.session.location} \n` +
-        `Estimated Ending: ${ctx.session.endTime} \n` +
-        `Description: ${ctx.session.description} \n\n` +
-        'Sending the post.... Please Wait \n'
-        )
-
-    var createdTime = admin.firestore.Timestamp;
-    var documentName = createdTime.fromMillis+"+"+functions.config().wave.uid;
-
-
-    const newfile = myBucket.file(`discussion/NTU/images/${functions.config().wave.uid}/${createdTime}.jpg`);
-
-    const imageLink = await bot.telegram.getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id)
+    //const newfile = myBucket.file(`discussion/NTU/images/${functions.config().wave.uid}/${createdTime}.jpg`);
+    ctx.session.imageFileId = ctx.message.photo[ctx.message.photo.length - 1].file_id
+    const imageLink = await bot.telegram.getFileLink(ctx.session.imageFileId)
     console.log(imageLink);
+    ctx.session.hasImage = true;
+    ctx.session.imageUrl = imageLink;
 
+    function upload(link, cb){
 
-    var fd = fs.createReadStream(imageLink)
-    fd.pipe(newfile.createWriteStream())
+      https.get(link, function(response) {
+        response.pipe(myImageStream);
+        console.log("start piping")
+        myImageStream.on('finish', function(){
+          
+  
+          myBucket.upload("/tmp/temp.jpg", {
+            destination: `${createdTime.toMillis()}.jpg`,
+            metadata: {
+              cacheControl: 'no-cache',
+            },
+          }).then(value =>{
+            
+            ctx.reply(
+              beginning +
+              `Food type:  ${ctx.session.foodType} \n` +
+              `Location: ${ctx.session.location} \n` +
+              `Estimated Ending: ${ctx.session.endTime} \n` +
+              `Description: ${ctx.session.description} \n` +
+              `Photo: Uploaded new photo\n\n` +
+        
+              'Note that you have reached the last step, you can replace the photo by new photo\n\nOR\n\n' + 
+              'you can enter other words to delete photo, after that,\n' + 
+              'you can click Next ▶ button to finish posting\n\n' + 
+              'By finishing, you agree to let bot to represent you to post to food channel and pls do not post misleading info',
+              { reply_markup: { keyboard: [['◀️ Back', 'Next ▶']], resize_keyboard: true, one_time_keyboard: true } }
+              )
+            cb(value)
+            return
 
-
-    var end = new Promise(function(resolve, reject) {
-      fd.on('finish', resolve);
-      fd.on('error', reject); // or something like that. might need to close `hash`
-    });
-
-    let sha1sum = await end;
-    console.log(sha1sum);
-
-    
-
-    var content = 
-      `Posted by: ${ctx.from.id} \n` +
-      `Food type:  ${ctx.session.foodType} \n` +
-      `Location: ${ctx.session.location} \n` +
-      `Estimated Ending: ${ctx.session.endTime} \n` +
-      `Description: ${ctx.session.description} \n`
-    let foodinfo = {
-      'creatorID' : 'Wave Admin',
-      'isAnonymous' : false,
-      'createdTime' : createdTime,
-      'likedPeople' : {},
-      'title' : `${ctx.session.foodType} at ${ctx.session.location}`,
-      'content' : content,
-      'category' : 'Food',
-      'creatorUID' : functions.config().wave.uid,
-      'hasImage' : true,
-      'imageUrl' : "",
-      'isPinned' : false,
-      'isHidden' : false,
-      'commentCount' : 0,
-      'likeCount' : 0,
-      'threadID' : documentName,
-
+          }).catch(err => {
+            console.log("error occured during uploading")
+            ctx.reply(
+              "Bot server error! We are sorrry!\n\n" + 
+              "You can try Send me a photo of the food agian or you can send any text if you want to skip have photo :"
+            )
+            cb(err)
+            
+          });
+          
+        })
+  
+      });
 
     }
 
-
-    ctx.reply(
-        'Post created!'
-        )
-
-    await ctx.scene.leave('getPic')
-    
-  
+    upload(imageLink, (result) => { console.log(result)})
+      
     
   })
 
@@ -452,11 +469,7 @@ getPic.on('text', async (ctx) => {
 bot.on('message', (ctx) => ctx.reply('To post new free food, enter /start'))
 
 
-
-//bot.launch()
-
-
-exports.bot = functions.https.onRequest(
+exports.bot2 = functions.https.onRequest(
   (req, res) => bot.handleUpdate(req.body, res)
 )
 
